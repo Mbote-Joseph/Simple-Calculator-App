c7a6cdb345f20402475889bb9bfd2612f4c604de
Joseph Mbote <mbotejoseph001@gmail.com>
2020-05-22 23:45:15 +0300

    first Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'AppCalculator';
+  title = 'Welcome to My AppCalculator';
+
+  public num1: number;
+  public num2: number;
+  public num3: number;
+
+  add(){
+    this.num3=this.num1 +this.num2;
+  }
+  sub(){
+    this.num3=this.num1 -this.num2;
+  }
+  multiply(){
+    this.num3=this.num1*this.num2;
+  }
+  divide(){
+    this.num3=this.num1/this.num2;
+  }
 }
